787a6e12ed02546b34c274183d56d9501f5d8b65
codehead <kbuimscd@gmail.com>
2015-11-19 23:49:15 -0700

    fix package.json and add link for bugs

--- Unit-Test.js
+++ Unit-Test.js
@@ -1,4 +1,18 @@
 /**
  * Created by kimbui on 10/9/15.
  */
+var yunbi = require("./yunbi-module");
+//TODO insert access and key to test
+var access = "";
+var key = "";
+var client = new yunbi(access,key);
+
+
+//client.getAllTickers(function(err,result){
+//    console.log(err,result)
+//})
+
+client.getTicker("ethcny","btccny",function(err,result){
+    console.log(err,result)
+})
 
